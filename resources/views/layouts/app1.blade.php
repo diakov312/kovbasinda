4956d797356d93d9dd0f970956882525c95cbffe
diakov312 <dkovbasin@gmail.com>
2018-08-08 11:24:39 +0300

    Added Yandex Metrica

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -25,6 +25,36 @@
         <link rel="stylesheet" href="css/main.css" />
     @endsection
     @yield('styles')
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter49897681 = new Ya.Metrika2({
+                        id:49897681,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks2");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/49897681" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
     @yield('content')
